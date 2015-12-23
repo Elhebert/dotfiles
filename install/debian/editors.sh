98720281255f3d7191012ff0c6edd498b1d0631d
dst <dst@europa>
2015-12-23 08:40:54 +0100

    Add atom/vs code for linux

--- install/debian/editors.sh
+++ install/debian/editors.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-if [ -d $HOME/.atom ]; then
+if ! test $(which atom) ; then
+	echo "Atom is not installed"
+	read -n 1 -p "Do you wish to install it [Yn] ?" atom
+	if [ -z "$atom" ] || [ $atom="y" ] || [ $atom="Y" ] ; then
+		wget https://github.com/atom/atom/releases/download/v1.3.2/atom-amd64.deb
+		dpkg -i atom-amd64.deb
+	fi
+fi
+
+if test $(which atom) ; then
 	echo "Atom installed. Importing config..."
-#	rm $HOME/.atom/config/config.cson
-#	ln -s $HOME/.dotfiles/editors/atom/config.cson.symlink $HOME/.atom/config/config.cson
+	rm $HOME/.atom/config/config.cson
+	ln -s $HOME/.dotfiles/editors/atom/config.cson.symlink $HOME/.atom/config/config.cson
 fi
 
-if [ -d $HOME/Library/Application\ Support/Code ]; then
+if test $(which code) ; then
 	echo "Code installed. Importing config..."
-#	rm $HOME/Library/Application\ Support/Code/User/settings.json
-#	ln -s $HOME/.dotfiles/editors/code/settings.symlink $HOME/Library/Application\ Support/Code/User/settings.json
+	rm $HOME/.config/Code/User/settings.json
+	ln -s $HOME/.dotfiles/editors/code/settings.symlink $HOME/.config/Code/User/settings.json
+else
+	echo "Code is not installed"
 fi
-
